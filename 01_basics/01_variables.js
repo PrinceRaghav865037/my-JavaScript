0263c6892796fdfdb981686e9742fa38d2c06ab4
PrinceRaghav865037 <princeraghav8650@gmail.com>
2025-08-31 18:13:18 +0000

    changes i made

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,10 +1,10 @@
 const accountId = 1234 
-let accountEmail = "chintu@facebook.com"
-var accountPassword = "ASDDSSFF"
+let accountEmail = "chintusite"
+var accountPassword = "678a"
 accountCity = "RaiBareilly"
 //accountId = 3456
-accountEmail = "Rohit@google.com"
-accountPassword = "GHJJJJKL"
+accountEmail = "Rohitogle"
+accountPassword = "123"
 accountCity = "Bareilly"
 /*Prefer not to use the var
 because issue in block scope and functional scope.
