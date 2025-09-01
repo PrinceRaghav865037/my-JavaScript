dff703d7378d011316c42f65e8251812f4bd7473
PrinceRaghav865037 <princeraghav8650@gmail.com>
2025-09-01 19:03:25 +0000

    it is next program

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all javascript code in newer version.
+// alert(3+3) // this is node js not the web browser .
+console.log(3+3)
+console.log("Raghav");
+let name = "shashwat"
+let age = 23
+let state;
+//number => 2 to power 53.
+// bigint
+// string => ""
+// boolean => true/false
+// null => standa lone value
+// undefined =>
+// symbol => unique
+// object
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+
