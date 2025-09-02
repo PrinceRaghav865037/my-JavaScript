686852d6cca8e4b9268242fda2f6008f70e0a3a3
PrinceRaghav865037 <princeraghav8650@gmail.com>
2025-09-02 13:37:14 +0000

    this is the third file

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = true;
+
+//console.log(typeof score)
+//console.log(typeof score)
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+ 
+//"33" = 33
+//"33bhd" = Nan (not an number )
+// true = 1 , false = 0
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//"" => false
+// "Prince" => true
+// 33(any number) or 1 => true
+//0 => false
+
+
