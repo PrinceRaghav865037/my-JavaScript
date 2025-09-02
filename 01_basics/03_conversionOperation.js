d1c0e79bba915dec43896d1d5db467a48f971806
PrinceRaghav865037 <princeraghav8650@gmail.com>
2025-09-02 14:12:07 +0000

    here i add some more conversion by default by compiler

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -18,5 +18,26 @@ console.log(booleanIsLoggedIn)
 // "Prince" => true
 // 33(any number) or 1 => true
 //0 => false
+let str1 = "himanshu"
+let str2 = "Pandey"
+console.log(str1 + str2)
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 3 + 5)
+console.log(3 + 2 + "2")
+
+let num1 , num2 ,num3 
+num1 = num2 = num3 = 2+2
+console.log(num1 + " " + num2 + " " + num3)
+let c = 405
+
+//++c
+console.log(c++)
+console.log(c)
+c++
+console.log(c)
+
+
+
 
 
