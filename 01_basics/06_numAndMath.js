07c675a168af77ee40ddefb16da5ee244cd81794
PrinceRaghav865037 <princeraghav8650@gmail.com>
2025-09-03 11:32:46 +0000

    Math and Num

---
+++ 01_basics/06_numAndMath.js
@@ -0,0 +1,32 @@
+const score = 444
+//console.log(score)
+const balance = new Number(406)
+//console.log(balance)
+//console.log(balance.toString().length)
+//console.log(balance.toFixed(2))
+const n = 156.8856
+//console.log(n.toPrecision(3))
+const hundreds = 10000000
+//console.log(hundreds.toLocaleString('en-IN'))
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++
+//maths
+// console.log(Math)
+// console.log(Math.abs(-8))
+// console.log(Math.round(22.7))
+// console.log(Math.ceil(4.2))
+// console.log(Math.ceil(4.7))
+// console.log(Math.floor(4.7))
+// console.log(Math.floor(4.1))
+
+console.log(Math.min(2 , 5 , 6 , 1))
+console.log(Math.max(2 , 5 , 6 , 1))
+ 
+console.log(Math.random())
+console.log(Math.random()*10 + 1)
+console.log(Math.floor((Math.random()*10) + 1))
+
+
+const min = 10
+const max = 20
+console.log(Math.floor((Math.random()*(max - min +1))) + min)
