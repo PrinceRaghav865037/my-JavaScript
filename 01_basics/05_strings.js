dafa4b977339eb58ca38c44a46bcaf8a56248344
PrinceRaghav865037 <princeraghav8650@gmail.com>
2025-09-03 10:32:49 +0000

    this all about strings in javascript

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,21 @@
+const name = "Shivam"
+const rollno = 58
+//console.log(name + rollno + "value")
+//console.log(`My name is ${name} and my rollno is ${rollno}`)
+const gameName  = new String('Tripathi rahul sharma')
+//console.log(gameName[0])
+// console.log(gameName.__proto__)
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(3))
+// console.log(gameName.indexOf('a'))
+//const newString = gameName.substring(0 , 4);
+//console.log(newString)
+const anotherString = gameName.slice(-1 , 4)
+console.log(anotherString)
+const newString = "    hitesh     "
+console.log(newString.trim())
+const url = "hiriuhifh@nkfnddnfn%20.com"
+console.log(url.replace('%20' , '-'))
+console.log(url.includes('rahul'))
+console.log(gameName.split(' '))
