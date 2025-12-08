9eaeb90fbdf87311eba7b3370574aa5000060312
PrinceRaghav865037 <princeraghav8650@gmail.com>
2025-12-08 06:04:51 +0000

    some mini hostiong

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -2,13 +2,33 @@ let a = 400
 if(true){
 let a = 10;
 const b = 30;
-console.log("INNER :" , a)
+//console.log("INNER :" , a)
 //var c = 50;
 }
 // for (let i = 0; i < array.length; i++) {
 //     const element = array[i];
     
 // }
-console.log(a)
+//console.log(a)
 //console.log(b)
-console.log(c)
+//console.log(c)
+// function one(){
+// const userName  = "Naitik"
+// function two(){
+//     const website = "pharmacy"
+//     console.log(userName)
+// }
+// //console.log(website)
+// two()
+// }
+// one()
+console.log(addOne(4)) // it can be accessed before initialization .
+function addOne(num){
+    return num+1
+}
+//addOne(5)
+console.log(addTwo(5));// it can't be accessed before initialization because this function hold into a const variable.
+const addTwo = function(num){
+ return num +2
+}
+// addTwo(5);
