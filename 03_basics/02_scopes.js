fa7e8f3f6a5cac82562e78943bb51c9bc6c3a841
PrinceRaghav865037 <princeraghav8650@gmail.com>
2025-12-08 05:34:56 +0000

    scopes in js

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,14 @@
+let a = 400
+if(true){
+let a = 10;
+const b = 30;
+console.log("INNER :" , a)
+//var c = 50;
+}
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
+console.log(a)
+//console.log(b)
+//console.log(c)
