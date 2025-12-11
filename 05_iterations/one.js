5da53e93abf32f21b0752e947b8038cc5ca91ef6
PrinceRaghav865037 <princeraghav8650@gmail.com>
2025-12-11 05:41:32 +0000

    for loop with break and continue

---
+++ 05_iterations/one.js
@@ -0,0 +1,20 @@
+//for
+// for(let i = 1; i<=10; i++){
+//     console.log(`it is the outer loop ${i}`)
+//     for(let j = 1; j<=10; j++){
+//         console.log(`${i} * ${j} = ${i*j}`)
+//     }
+// }
+// let arr = ["super" , "beauty" , "dull"]
+// console.log(arr.length)
+// for(let i = 0; i<arr.length; i++){
+//     console.log(arr[i])
+// }
+for (let i = 1; i <= 20; i++) {
+    if(i==5){
+        console.log("detected 5")
+        continue;
+    }
+    console.log(i)
+    
+}
