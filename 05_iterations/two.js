beee96cae403ba493bcd23565f0cb08d8a3c05a3
PrinceRaghav865037 <princeraghav8650@gmail.com>
2025-12-11 12:08:12 +0000

    while and do-while loop overview

---
+++ 05_iterations/two.js
@@ -0,0 +1,17 @@
+// while and do while loop 
+let index = 0;
+while(index<=10){
+    console.log(`value of index is ${index}`)
+    index += 2
+}
+let arr = [9, 6 , 3, 10]
+let i = 0;
+while(i<arr.length){
+    console.log(`value at index ${i} is ${arr[i]}`)
+    i++
+}
+let score = 11;
+do{
+    console.log(score);
+    score++;
+}while(score<=10)
