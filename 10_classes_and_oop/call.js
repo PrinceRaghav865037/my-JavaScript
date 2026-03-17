6c4c2c8f29a8044bc1437e6cf76fe5e52c34d170
PrinceRaghav865037 <princeraghav8650@gmail.com>
2026-03-17 06:54:43 +0000

    call and this

---
+++ 10_classes_and_oop/call.js
@@ -0,0 +1,15 @@
+function setUsername(username){
+    // complex DB calls 
+    this.username = username
+    console.log("called");
+    
+}
+
+function createUser(username , email , password){
+    setUsername.call(this,username)
+    // .call refernce hold karta hai 
+    this.email = email
+    this.password = password
+}
+const chai = new createUser("chai" , "jkhfiuhfw" , "123")
+console.log(chai)
